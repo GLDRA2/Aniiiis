0c868d69197f3a70f4b52b5698819355dcc71fb3
xxanisxx <aniscaca3@gmail.com>
2020-12-25 23:35:32 +0100

    private groupe message

--- symfony/src/Entity/Groupe.php
+++ symfony/src/Entity/Groupe.php
@@ -54,9 +54,15 @@ class Groupe
      */
     private $cours;
 
+    /**
+     * @ORM\OneToMany(targetEntity=Message::class, mappedBy="groupe", orphanRemoval=true)
+     */
+    private $messages;
+
     public function __construct()
     {
         $this->users = new ArrayCollection();
+        $this->messages = new ArrayCollection();
     }
 
     public function getId(): ?int
@@ -123,6 +129,36 @@ class Groupe
     public function setCours(Cour $cours): self
     {
         $this->cours = $cours;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection|Message[]
+     */
+    public function getMessages(): Collection
+    {
+        return $this->messages;
+    }
+
+    public function addMessage(Message $message): self
+    {
+        if (!$this->messages->contains($message)) {
+            $this->messages[] = $message;
+            $message->setGroupe($this);
+        }
+
+        return $this;
+    }
+
+    public function removeMessage(Message $message): self
+    {
+        if ($this->messages->removeElement($message)) {
+            // set the owning side to null (unless already changed)
+            if ($message->getGroupe() === $this) {
+                $message->setGroupe(null);
+            }
+        }
 
         return $this;
     }
--- symfony/src/Form/CourType.php
+++ symfony/src/Form/CourType.php
@@ -8,8 +8,6 @@ use App\Entity\Category;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
-
-
 use Symfony\Component\Form\Extension\Core\Type\FileType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\OptionsResolver\OptionsResolver;
